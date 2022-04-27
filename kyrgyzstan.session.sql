e667518c16208018dd0d386deca17b6ce4be85fa
adilkaaa <adilkhanakakov@gmail.com>
2022-04-27 16:53:02 +0600

    add kg-db

---
+++ kyrgyzstan.session.sql
@@ -0,0 +1,16 @@
+-- CREATE TABLE developers(id serial, name VARCHAR,population int);
+
+-- INSERT INTO developers(name, population) VALUES ('Chui',100000),
+-- ('Osh',200000),('Naryn',300000);
+
+
+-- ALTER TABLE developers ADD COLUMN teams varchar;
+
+-- ALTER TABLE developers RENAME column population to participants;
+
+-- DELETE FROM developers where participants = 300000;
+
+-- UPDATE developers set participants = 7000 where participants > 8000;
+
+SELECT * FROM developers;
+
