e3b50d07724695e2cac3a48ffa5cc543ecf75d30
adilkaaa <adilkhanakakov@gmail.com>
2022-04-27 19:38:33 +0600

    add companies_db

---
+++ companies.session.sql
@@ -0,0 +1,29 @@
+-- CREATE TABLE phones(id serial, name varchar, price int);
+
+-- insert into phones(name,price) values ('Iphone',1000);
+
+
+-- alter table phones add column country varchar;
+
+-- insert into phones(name, price, country) values('Samsung',800,'Korea'),
+-- ('Nokia',200,'Kyrgyzstan'),('MI',1,'Uzbekistan'),
+-- ('Google',0,'USA');
+-- select * from phones;
+
+
+
+-- CREATE TABLE cars(id serial, name VARCHAR,price int DEFAULT 300);
+
+-- INSERT INTO cars(name,price) values('MB',1000);
+
+-- ALTER TABLE cars ADD COLUMN country VARCHAR;
+
+-- INSERT INTO cars(name,price,country) values('Audi',300,'Germany'),
+-- ('BMW',11800,'Germany'),('Tulpar',1111800,'Kyrgyzstan');
+
+-- UPDATE cars set country = 'Germany' where name = 'MB';
+-- SELECT * FROM cars;
+
+
+
+
